3d1d91e2240b8ad359bc13cb2424f93d5a63b8d8
Ujjawal Khadanga <b20139@students.iitmandi.ac.in>
2023-12-04 06:18:29 +0530

    added speech to text

--- scripts/speech_to_text.py
+++ scripts/speech_to_text.py
@@ -1,35 +1,50 @@
 #!/usr/bin/env python3
 
 import rospy
-from speech_recognition import Recognizer, Microphone
-from std_msgs.msg import String
-from turtle_gpt.srv import audioToText
+import sounddevice as sd
+import rospkg
+import os
+import vosk
+import base64
+import json
 
-def speech_to_text_callback(req):
-    # Speech-to-text conversion logic
-    recognizer = Recognizer()
-    microphone = Microphone()
+from turtle_gpt.srv import speechToText, speechToTextResponse
 
-    with microphone as source:
-        print("Say something...")
-        audio_data = recognizer.listen(source)
+class SpeechToText:
+    def __init__(self):
+        rospy.init_node('stt_node')
+        self.service = rospy.Service("stt", speechToText, self.handler)
 
-    try:
-        text_result = recognizer.recognize_google(audio_data)
-        print("Text from Speech-to-Text:", text_result)
-        return text_result
-    except recognizer.UnknownValueError:
-        print("Speech-to-Text could not understand audio")
-        return ""
-    except recognizer.RequestError as e:
-        print("Could not request results from Speech-to-Text service; {0}".format(e))
-        return ""
+        ## vsok models (https://alphacephei.com/vosk/models)
+        model_name = "vosk-model-small-en-us-0.15"
+        rospack = rospkg.RosPack()
+        package_path = rospack.get_path('turtle_gpt')
+        models_dir = os.path.join(package_path, 'models')
+        model_path = os.path.join(models_dir, model_name)
+        self.model = vosk.Model(model_path)
 
-def speech_to_text_node():
-    rospy.init_node('speech_to_text_node', anonymous=True)
-    rospy.Service('/speech_to_text_conversion', audioToText, speech_to_text_callback)
-    print("Speech-to-Text Node is ready.")
-    rospy.spin()
+        input_dev_num = sd.query_hostapis()[0]['default_input_device']
+        device_info = sd.query_devices(input_dev_num, 'input')
+        self.samplerate = int(device_info['default_samplerate'])
+
+    
+    def speechRecognition(self, audio_data):
+        rec = vosk.KaldiRecognizer(self.model, self.samplerate)
+        result_text = ""
+        if rec.AcceptWaveform(audio_data):
+            result = rec.FinalResult()
+            diction = json.loads(result)
+            if len(diction["text"]) > 2:
+                result_text = diction["text"]
+            rec.Reset()
+        return result_text
+
+    def handler(self, data):
+        audio = base64.b64decode(data.b64_audio)
+        result_text = self.speechRecognition(audio)
+        # res = speechToTextResponse(result_text)
+        return result_text
 
 if __name__ == '__main__':
-    speech_to_text_node()
+    SpeechToText()
+    rospy.spin()
