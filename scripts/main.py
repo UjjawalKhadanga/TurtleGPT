a5aa8dfa327d98a0d104012d7dc98b7f3e0fed60
Ujjawal Khadanga <b20139@students.iitmandi.ac.in>
2023-12-05 00:06:35 +0530

    controler bug fixes

--- scripts/main.py
+++ scripts/main.py
@@ -10,17 +10,19 @@ def main():
         inp = input("Enter the command for turtlesim (Enter q to exit): ")
         if inp == 'q':
             break
+
         rospy.wait_for_service('/GPTPrompt')
         GPTService = rospy.ServiceProxy('/GPTPrompt', GPTPrompt)
         res = GPTService(inp)
-
         result = String()
         result.data = res.response
+
+        # result = String()
         # result.data = '''[
-        #     {"action": "rotateByTheta", "params": { "theta": 2, "direction": "clockwise" }},
-        #     {"action": "moveByDistance", "params": { "distance" : 1 }},
-        #     {"action": "goToGoal", "params": { "goal_x" : 1, "goal_y": 1, "goal_theta": 1.57  }}
+        #     {"action": "goToGoal", "params": { "goal_x" : 1, "goal_y": 1, "goal_theta": 1.57  }},
+        #     {"action": "goToGoal", "params": { "goal_x" : 5, "goal_y": 9, "goal_theta": 2  }}
         # ]'''
+
         publisher.publish(result)
 
 
